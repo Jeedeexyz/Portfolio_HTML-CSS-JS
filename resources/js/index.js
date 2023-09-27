12b66a87d91bbe5694ffe36f4903f410bc0c49d0
Jeedeexyz <123000633+Jeedeexyz@users.noreply.github.com>
2023-09-27 11:47:35 +0500

    resolved nav bar issue and added js event listener

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,13 @@
 
-$(".menu").click(()=>{
-    $("#nav").toggle("hide")
+
+const icon = document.querySelector("#dropDown")
+
+icon.addEventListener("click",()=>{
+    console.log("clicked")
+    document.querySelector("#nav").classList.toggle("show")
 })
 
-$("#nav li").click(()=>{
-    $("#nav").addClass("hide")
-})
+
+// $("#nav li").click(()=>{
+//     $("#nav").toggle("hide")
+// })
